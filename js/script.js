10844c217d06d689fb3212945d41e49fbaaeab24
PaulaVanesa <89751622+PaulaVanesa@users.noreply.github.com>
2023-03-01 12:20:00 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 40, //Velocidad de tipeo de cada palabra
+    backSpeed:20, //Velocidad de borrado
+    loop:true,
+    backDelay: 2500, //Tiempo de espera en la pabra antes que se borre
+    smartBackspace: true,
+  });
